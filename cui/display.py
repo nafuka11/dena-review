fe1cf689655ccc226612813365bc027593eef66b
nfukada <nfukada@student.42tokyo.jp>
2021-12-14 22:25:54 +0900

    feat(display): improve appearance of board

--- cui/display.py
+++ cui/display.py
@@ -1,24 +1,28 @@
 from logic.board import Board
 from logic.cell import CellState
 
-CELL_CHARACTER = {CellState.PLAYER: "o", CellState.OPPONENT: "x", CellState.EMPTY: "."}
+CELL_CHARACTER = {CellState.PLAYER: "O", CellState.OPPONENT: "X", CellState.EMPTY: "."}
 
 PLAYER_NAME = {True: "Player1", False: "Player2"}
 
 ESC_CLEAR_SCREEN = "\033[2J"
+ESC_CURSOR_UP = "\033[1A"
+ESC_CLEAR_LINE = "\033[2K"
 
 
 def display_board(board: Board) -> None:
-    print(ESC_CLEAR_SCREEN)
+    _clear_screen()
+    print("+" + "-" * (board.size.x * 2 - 1) + "+")
     for y in range(board.size.y):
-        for x in range(board.size.x):
-            _display_cell(board.cells[y][x])
+        print("|", end="")
+        print(
+            " ".join([CELL_CHARACTER[board.cells[y][x]] for x in range(board.size.x)]),
+            end="",
+        )
+        print("|", end="")
         print()
-    print("".join([str(x) for x in range(0, board.size.x)]))
-
-
-def _display_cell(cell: CellState) -> None:
-    print(CELL_CHARACTER[cell], end="")
+    print("+" + "-" * (board.size.x * 2 - 1) + "+")
+    print(" " + " ".join([str(x) for x in range(1, board.size.x + 1)]))
 
 
 def display_turn_info(is_player_turn: bool) -> None:
@@ -31,9 +35,16 @@ def display_turn_info(is_player_turn: bool) -> None:
 
 
 def display_input_message(min_value: int, max_value: int) -> None:
+    _clear_line()
     print(f"Please input index({min_value}-{max_value}): ", end="")
 
 
 def display_winner_message(is_player_turn: bool) -> None:
     print()
     print(f"{PLAYER_NAME[is_player_turn]} wins!")
+
+def _clear_screen() -> None:
+    print(ESC_CLEAR_SCREEN)
+
+def _clear_line() -> None:
+    print(f"{ESC_CURSOR_UP}{ESC_CLEAR_LINE}", end="")
