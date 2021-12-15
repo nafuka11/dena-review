fbd3bf828f1a3c8ed62b3124e0e371e065e49bc0
nfukada <nfukada@student.42tokyo.jp>
2021-12-16 01:10:58 +0900

    feat(main.py): select CUI or GUI by argument

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-from cui.game import Game
+from argparse import ArgumentParser, Namespace
+
+from cui.game import CUIGame
+from gui.game import GUIGame
 
 
 def main() -> None:
-    game = Game()
-    game.run()
+    args = parse_args()
+    if args.cui:
+        CUIGame().run()
+    else:
+        GUIGame().run()
+
+
+def parse_args() -> Namespace:
+    parser = ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-c", "--cui", action="store_true", help="play CUI game")
+    group.add_argument(
+        "-g", "--gui", action="store_true", help="play GUI game", default=True
+    )
+    return parser.parse_args()
 
 
 main()
